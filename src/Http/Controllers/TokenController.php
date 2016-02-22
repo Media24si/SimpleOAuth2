21396d1f755420a21e38f6ce7162652c9babac66
Bostjan Oblak <bostjan@muha.cc>
2016-02-22 09:05:31 +0100

    pass request object

--- src/Http/Controllers/TokenController.php
+++ src/Http/Controllers/TokenController.php
@@ -10,10 +10,10 @@ use OAuth2\OAuth2ServerException;
 class TokenController extends BaseController
 {
 
-    public function token(OAuth2 $oauth)
+    public function token(OAuth2 $oauth, Request $request)
     {
         try {
-            return $oauth->grantAccessToken();
+            return $oauth->grantAccessToken($request);
         } catch (OAuth2ServerException $oauthError) {
             return $oauthError->getHttpResponse();
         }
